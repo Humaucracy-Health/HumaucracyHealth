9e816fc0790d940725a12e03749294380181ac3a
Melinda Santana <melinda.santana@gmail.com>
2019-10-16 20:26:04 -0700

    updating db and npm files
    updating db and npm files

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,11 @@
+DROP DATABASE patientInfo_db;
+CREATE DATABASE patientInfo_db;
+
 USE patientInfo_db;
 ​
-CREATE TABLE  patients (
+CREATE TABLE patients (
     id INT NOT NULL AUTO_INCREMENT, /* NOT NULL means that this column can not be empty, and it is called a constraint */
-    pat_name VARCHAR(255),
+    patient_name VARCHAR(255),
     PRIMARY KEY(id) /* if you don't do line 11, you get an error */
 );
 
@@ -15,32 +18,19 @@ CREATE TABLE  charts (
 );
 
 CREATE TABLE appointments (
-	id INT NOT NULL AUTO_INCREMENT,
-	doc_name VARCHAR(255),
-	patient_name VARCHAR(255),
-	vis_date DATE,
-	vis_time TIME,
-	-- FOREIGN KEY (pat_id) REFERENCES patients(id),
-	-- FOREIGN KEY (doc_id) REFERENCES doctors(id),
+id INT NOT NULL AUTO_INCREMENT,
+    doc_name VARCHAR(255),
+    loc_name VARCHAR(255),
+    pat_name VARCHAR(255),
+    reason VARCHAR(255),
+    note VARCHAR(255),
+    vis_date DATE,
+    vis_time TIME,
 PRIMARY KEY(id)
 );
 
-CREATE TABLE visit_history (
+CREATE TABLE payments (
     id INT NOT NULL AUTO_INCREMENT, /* NOT NULL means that this column can not be empty, and it is called a constraint */
-    appt_id INT NOT NULL,
-    note VARCHAR(255),
-    -- FOREIGN KEY (pat_id) REFERENCES patients(id),
-    -- FOREIGN KEY (doc_id) REFERENCES doctors(id),
-    FOREIGN KEY (appt_id) REFERENCES appointments(id),
-    PRIMARY KEY(id) 
-);
-
-CREATE TABLE visit_history (
-    id INT NOT NULL AUTO_INCREMENT, /* NOT NULL means that this column can not be empty, and it is called a constraint */
-    appt_id INT NOT NULL,
-    note VARCHAR(255),
-    -- FOREIGN KEY (pat_id) REFERENCES patients(id),
-    -- FOREIGN KEY (doc_id) REFERENCES doctors(id),
-    FOREIGN KEY (appt_id) REFERENCES appointments(id),
+    amount DECIMAL(10,2),    
     PRIMARY KEY(id) 
 );
